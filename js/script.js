14129f977715e265e722df2ff1271fb953cd6c2a
CauBitten <caua.fb@gmail.com>
2024-04-11 17:57:16 -0300

    Repaginação do Site

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ icon.onclick = function() {
     document.body.classList.toggle("dark");
 
     if (document.body.classList.contains("dark")) {
-        icon.src = "components/sun.png";
+        icon.src = "components/logow.png";
     } else {
-        icon.src = "components/moon.png";
+        icon.src = "components/logob.png";
     }
 }
