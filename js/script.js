0cfd8aa767469f3cfb7fe107f95c136c9deef266
CauBitten <caua.fb@gmail.com>
2024-04-12 02:26:10 -0300

    Update Site

--- js/script.js
+++ js/script.js
@@ -5,7 +5,20 @@ icon.onclick = function() {
 
     if (document.body.classList.contains("dark")) {
         icon.src = "components/logow.png";
+
+        for (let i = 0; i < 6; i++) {
+            let gitIcon = document.getElementById("git_icon" + i);
+
+            gitIcon.src = "components/githubw.png"
+        }
+
     } else {
         icon.src = "components/logob.png";
+
+        for (let i = 0; i < 6; i++) {
+            let gitIcon = document.getElementById("git_icon" + i);
+
+            gitIcon.src = "components/githubb.png"
+        }
     }
 }
