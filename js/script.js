64d4a77cbb4deb3bc4c246f038eb2011ed16a063
Cauã Bittencourt <caua.fb@gmail.com>
2024-07-21 11:06:39 -0300

    Repaginação do Site

--- js/script.js
+++ js/script.js
@@ -1,24 +1,4 @@
-let icon = document.getElementById("icon");
-
-icon.onclick = function() {
-    document.body.classList.toggle("dark");
-
-    if (document.body.classList.contains("dark")) {
-        icon.src = "components/logow.png";
-
-        for (let i = 0; i < 6; i++) {
-            let gitIcon = document.getElementById("git_icon" + i);
-
-            gitIcon.src = "components/githubw.png"
-        }
-
-    } else {
-        icon.src = "components/logob.png";
-
-        for (let i = 0; i < 6; i++) {
-            let gitIcon = document.getElementById("git_icon" + i);
-
-            gitIcon.src = "components/githubb.png"
-        }
-    }
+function toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.classList.toggle('active')
 }
